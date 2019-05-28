1abc0abf87170a69f524d90a118a35140f8eccb9
Florian Wallner <florian.wallner@exxcellent.de>
2019-05-28 22:39:44 +0200

    change port to 8080 (google app engine default port)

--- app/server.py
+++ app/server.py
@@ -55,5 +55,5 @@ async def analyze(request):
     })
 
 if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=80)
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
 
